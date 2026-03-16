2ecbfb595b0cb4d681bc06326f51c05ab5fc3633
BrunoMaiolo <brunosj@hotmail.it>
2026-03-16 15:28:27 +0100

    aggiunto il percorso di visualizzazione per un singolo film

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -12,4 +12,16 @@ function index(req, res) {
 
 }
 
-module.exports = { index };
+function show(req, res) {
+
+  const id = req.params.id;
+  const sql = "SELECT * FROM movies WHERE id = ?";
+
+  connection.query(sql, [id], (err, results) => {
+    if (err) throw err;
+
+    res.json(results[0]);
+  });
+}
+
+module.exports = { index, show };
--- routers/moviesRouter.js
+++ routers/moviesRouter.js
@@ -5,4 +5,6 @@ const moviesController = require("../controllers/moviesController");
 
 router.get("/", moviesController.index);
 
+router.get('/:id', moviesController.show);
+
 module.exports = router;
