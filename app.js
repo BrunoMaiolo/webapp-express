cdf2e1d62eef4e1fbbdc24b100fcd9a32d6b9aa7
BrunoMaiolo <brunosj@hotmail.it>
2026-03-17 16:08:41 +0100

    installato CORS

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const cors = require("cors");
+
 const moviesRouter = require("./routers/moviesRouter");
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.use("/movies", moviesRouter);
 
